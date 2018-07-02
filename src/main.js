83d4c27eb1b3ac4a034d600728b42e9a5d3893fc
Dima Storozhuk <dmutro.storozhuk@gmail.com>
2018-07-02 20:29:30 +0300

    Add target element validation before attach input mask to the target.

--- src/main.js
+++ src/main.js
@@ -4,11 +4,35 @@ import App from './App.vue'
 import Nebo15Mask from 'nebo15-mask'
 
 Vue.directive('rocket-mask', {
-  bind (el) {
-    // eslint-disable-next-line
-    console.log('directive bind hook')
-    // eslint-disable-next-line
-    console.log(el)
+  bind (el, binding) {
+    const type = el.getAttribute('type');
+
+    // Validate the right HTML element.
+    if (el.tagName.toLowerCase() !== 'input') {
+      throw new TypeError("Unsupported HTML element type");
+    }
+
+    // List supported input types.
+    const supportedInputTypes = [
+      'email',
+      'text',
+      'tel',
+      'number'
+    ];
+
+    // Validate in the current element is input.
+    if (!supportedInputTypes.includes(type)) {
+      throw new TypeError("Unsupported input type");
+    }
+
+    // Validate required properties.
+    if (!binding.hasOwnProperty('value')) {
+      throw new TypeError("Please provide the configuration object as \"v-rocket-mask\" directive value.");
+    }
+    if (!binding.value.hasOwnProperty('mask')) {
+      throw new TypeError("The \"v-rocket-mask\" directive value must contain at least \"mask\" property.");
+    }
+
   },
   inserted (el) {
     // eslint-disable-next-line
